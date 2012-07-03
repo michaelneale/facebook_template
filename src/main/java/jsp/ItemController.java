84bdf271b7fc1174c68ca2ea30be9dba81bd70e6
unknown <Asus@Asus-PC.(none)>
2012-07-03 11:14:58 -0400

    added author

--- src/main/java/jsp/ItemController.java
+++ src/main/java/jsp/ItemController.java
@@ -1,26 +1,27 @@
 package jsp;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
-import java.lang.StringBuffer;
+import java.lang.*;
+import java.net.*;
+
 import java.security.SecureRandom;
 import java.math.BigInteger;
-import java.net.URLConnection;
-import java.net.URL;
+import org.w3c.dom.Document;
+import com.facebook.api.*;
 
+import net.sf.json.JSONObject;
+import net.sf.json.JSONSerializer;
 import javax.persistence.Persistence;
 import javax.persistence.PersistenceUnit;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.w3c.dom.Document;
-
-import com.facebook.api.*;
 
 public class ItemController  extends javax.servlet.http.HttpServlet implements javax.servlet.Servlet {
 
@@ -44,23 +45,58 @@ public class ItemController  extends javax.servlet.http.HttpServlet implements j
 
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		if (request.getParameter("token") != null) {
-			doPost(request,response);
-		} else {
+
+		HttpSession session = request.getSession(true);
+		Boolean valid = false;
+
+		// If token is valid
+		if (session.getValue("token") != null) {
+			String info = "https://graph.facebook.com/me?access_token=" + session.getValue("token");
+			// Check if token is valid, if so, valid = true
+		}
+
+		if (valid) { 
+			doPost(request, response);
+		} else {	
 			if (request.getParameter("state") != null) {
 				if (request.getParameter("code") != null) {
+
+					// Access authorized, getting auth token.
 					String code = request.getParameter("code");
 					String tokenReq = "https://graph.facebook.com/oauth/access_token?client_id=" + APP_ID + "&redirect_uri=" + REDIRECT_URL + "&client_secret=" + APP_SECRET +"&code=" + code;
+					
 					URL url = new URL(tokenReq);
-					URLConnection connection = url.openConnection();
-					connection.connect();
-					String token = connection.getHeaderField("access_token");
-					request.setAttribute("token", token);
-					request.getRequestDispatcher("/").forward(request, response);
+					HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+					connection.setRequestMethod("GET");
+					connection.setDoOutput(true);
+          			connection.connect();
+
+					if (connection.getResponseCode() == 200) {
+						BufferedReader rd  = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+          				StringBuffer sb = new StringBuffer();
+          				String line;
+          				while ((line = rd.readLine()) != null)
+              				sb.append(line + '\n');
+
+          				String body = sb.toString();
+						String token = body.split("&")[0].split("=")[1];
+
+						session.setAttribute("token", token);
+
+						doPost(request, response);
+					} else {
+						request.setAttribute("error", "Failed to retrieve token.");
+						request.getRequestDispatcher("jsp/error.jsp").forward(request, response);
+					}
+
+
 				} else {
-					request.getRequestDispatcher("jsp/denied.jsp").forward(request, response);
+					// Access was denied to the application
+					request.setAttribute("error", "You denied access to this Facebook application.");
+					request.getRequestDispatcher("jsp/error.jsp").forward(request, response);
 				}	
 			} else {
+				// Redirecting to Facebook splash page
 				String facebook;
 				facebook = "https://www.facebook.com/dialog/oauth?client_id=" + APP_ID + "&redirect_uri=" + REDIRECT_URL + "&state=" + getNewState();
 				response.sendRedirect(facebook);
@@ -70,6 +106,11 @@ public class ItemController  extends javax.servlet.http.HttpServlet implements j
 
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
+
+		if (request.getSession() != null) {
+
+				HttpSession session = request.getSession();
+				Boolean valid = true;
 
 				if (emf == null)
 					emf = Persistence.createEntityManagerFactory("itemManager");
@@ -89,33 +130,69 @@ public class ItemController  extends javax.servlet.http.HttpServlet implements j
 						String name = request.getParameter("name");
 						String comment = request.getParameter("comment");
 						if (name != null) {
+
 							em.getTransaction().begin();
-							//TODO Get facebook name! If token doesn't work, do a doGet
-							String author = "me";
-							em.persist(new Item(name, comment, author));
-							em.getTransaction().commit();
+							String info = "https://graph.facebook.com/me?access_token=" + (String) session.getAttribute("token");
+							URL url = new URL(info);
+							HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+							connection.setRequestMethod("GET");
+							connection.setDoOutput(true);
+		          			connection.connect();
+
+							if (connection.getResponseCode() == 200) {
+								BufferedReader rd  = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		          				StringBuffer sb = new StringBuffer();
+		          				String line;
+		          				while ((line = rd.readLine()) != null)
+		              				sb.append(line + '\n');
+
+		          				String body = sb.toString();
+		          				JSONObject json = (JSONObject) JSONSerializer.toJSON(body);
+		          				String author = json.getString("name");
+
+								em.persist(new Item(name, comment, author));
+								em.getTransaction().commit();
+							} else {
+								valid = false;
+							}
 						}
 					}
 
-				List<Item> itemList = em.createQuery("SELECT i FROM Item i", Item.class).getResultList();
-				StringBuffer sb = new StringBuffer();
-				for (Item item: itemList) { 
-					sb.append("<div class=\"item\">\n<tr><td>");
-					sb.append(item.getName());
-					sb.append("</td><td>");
-					sb.append(item.getComment());
-					sb.append("</td><td>");
-					sb.append(item.getAuthor());
-					sb.append("</td></tr>\n</div>\n");
-	    		}
-	    		String out = sb.toString();
-				request.setAttribute("items", out);
-				request.getRequestDispatcher("jsp/index.jsp").forward(request, response);
+					if (valid) {
+						List<Item> itemList = em.createQuery("SELECT i FROM Item i", Item.class).getResultList();
+						StringBuffer sb = new StringBuffer();
+						for (Item item: itemList) { 
+							sb.append("<div class=\"item\">\n<tr><td>");
+							sb.append(item.getName());
+							sb.append("</td><td>");
+							sb.append(item.getComment());
+							sb.append("</td><td>");
+							sb.append(item.getAuthor());
+							sb.append("</td></tr>\n</div>\n");
+			    		}
 
+			    		String out = sb.toString();
+						request.setAttribute("items", out);
+						request.getRequestDispatcher("jsp/index.jsp").forward(request, response);
+					} else {
+						request.setAttribute("error", "Your token has expired.");
+						request.getRequestDispatcher("jsp/error.jsp").forward(request, response);
+					}
+				} catch (PersistenceException e) {
+					request.setAttribute("error", "The data you entered is invalid, please make sure you did not use duplicate names.");
+					request.getRequestDispatcher("jsp/error.jsp").forward(request, response);
+				} catch (IllegalArgumentException e) {
+					request.setAttribute("error", "Cannot delete an item that doesn't exist.");
+					request.getRequestDispatcher("jsp/error.jsp").forward(request, response);
 				} finally {
 					if (em.getTransaction().isActive())
 						em.getTransaction().rollback();
 					em.close();
 				}
+		} else {
+			request.setAttribute("error", "Your session has expired.");
+			request.getRequestDispatcher("jsp/error.jsp").forward(request, response);
+		}
+
 	}
 }
